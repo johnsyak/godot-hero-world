3ac9056d474805e98d32ecd765c55bfaba10178c
sraven24 <ksanders236@gmail.com>
2023-07-08 13:48:39 -0400

    working on slime

--- godot project/enemies/slime/slime.gd
+++ godot project/enemies/slime/slime.gd
@@ -4,34 +4,42 @@ extends CharacterBody2D
 @export var limit = 0.5
 @export var endPoint: Marker2D
 
+@onready var animations = $AnimationPlayer
+
 var startPosition
 var endPosition
 
 func _ready():
 	pass
 	
-func changeDirection():
-	var tempEnd = endPosition
-	endPosition = startPosition
-	startPosition = tempEnd
+#func changeDirection():
+	#var tempEnd = endPosition
+	#endPosition = startPosition
+	#startPosition = tempEnd
 	
-func updateVelocity():
-	var moveDirection = (endPosition - position)
-	if moveDirection.length() < limit:
-		changeDirection()
+#func updateVelocity():
+	#var moveDirection = (endPosition - position)
+	#if moveDirection.length() < limit:
+		#changeDirection()
 		
-	velocity = moveDirection.normalized()*speed
+	#velocity = moveDirection.normalized()*speed
 	
 func updateAnimation():
 	if velocity.length() == 0:
-		pass
+		if animations.is_playing():
+			animations.stop()
 	else:
-		var direction = "Down"
-		if velocity.x < 0: direction = "Left"
-		elif velocity.x > 0: direction = "Right"
-		elif velocity.y < 0:direction = "Up"
+		var direction = "_down"
+		if velocity.x < 0: direction = "_left"
+		elif velocity.x > 0: direction = "_right"
+		elif velocity.y < 0:direction = "_up"
+		
+		animations.play("walk"+direction)
 		
 func _physics_process(delta):
+	#updateVelocity()
 	move_and_slide()
+	updateAnimation()
+	
 
 	
