12bf1ddadfc0217b078257beabd5603608544a85
JohnY <johnyak381@gmail.com>
2023-07-08 16:22:20 -0400

    fixed variable declaration cleanup

--- godot project/enemies/lizard/lizard.gd
+++ godot project/enemies/lizard/lizard.gd
@@ -10,41 +10,41 @@ extends CharacterBody2D
 @onready var enemy_raycast = $EnemyRaycast
 const projectilePath = preload("res://projectiles/projectile.tscn")
 
-var _tether_pos
-var _start_pos
-var _end_pos
-var _rng = RandomNumberGenerator.new()
-var _CONST_TILE_SIZE = 16
+var tether_pos
+var start_pos
+var end_pos
+var rng = RandomNumberGenerator.new()
+var CONST_TILE_SIZE = 16
 
 func _ready():
-	tether_max = tether_max * _CONST_TILE_SIZE 
-	_tether_pos = position
-	_start_pos = _tether_pos
-	_end_pos = _start_pos + Vector2(0, tile_move_dist * _CONST_TILE_SIZE)
-	_rng.randomize()
-	_end_pos = _start_pos - Vector2(_get_rand_range_threshold() * _CONST_TILE_SIZE, _get_rand_range_threshold() * _CONST_TILE_SIZE)
+	tether_max = tether_max * CONST_TILE_SIZE 
+	tether_pos = position
+	start_pos = tether_pos
+	end_pos = start_pos + Vector2(0, tile_move_dist * CONST_TILE_SIZE)
+	rng.randomize()
+	end_pos = start_pos - Vector2(_get_rand_range_threshold() * CONST_TILE_SIZE, _get_rand_range_threshold() * CONST_TILE_SIZE)
 
 func _update_velocity():
-	var moveDirection = _end_pos - position
+	var moveDirection = end_pos - position
 	if moveDirection.length() < limit:
-		position = _end_pos
+		position = end_pos
 		_change_direction()
 	velocity = moveDirection.normalized()*speed
 	
 func _change_direction():
-	var tempEnd = _end_pos
-	var tether = Vector2((_tether_pos.x + tether_max), (_tether_pos.y + tether_max))
-	if(_start_pos.x > 0 and _start_pos.x > tether.x):
+	var tempEnd = end_pos
+	var tether = Vector2((tether_pos.x + tether_max), (tether_pos.y + tether_max))
+	if(start_pos.x > 0 and start_pos.x > tether.x):
 		_resetPos()
-	elif(_start_pos.y > 0 and _start_pos.y > tether.y):
+	elif(start_pos.y > 0 and start_pos.y > tether.y):
 		_resetPos()
-	elif(_start_pos.x < 0 and _start_pos.x < (_tether_pos.x + -abs(tether.x))):
+	elif(start_pos.x < 0 and start_pos.x < (tether_pos.x + -abs(tether.x))):
 		_resetPos()
-	elif(_start_pos.y < 0 and _start_pos.y < (_tether_pos.y + -abs(tether.y))):
+	elif(start_pos.y < 0 and start_pos.y < (tether_pos.y + -abs(tether.y))):
 		_resetPos()
 	else:
-		_start_pos = tempEnd - Vector2((_get_rand_range_threshold()) * _CONST_TILE_SIZE, (_get_rand_range_threshold()) * _CONST_TILE_SIZE)
-	_end_pos = _start_pos
+		start_pos = tempEnd - Vector2((_get_rand_range_threshold()) * CONST_TILE_SIZE, (_get_rand_range_threshold()) * CONST_TILE_SIZE)
+	end_pos = start_pos
 
 func _update_animation():
 	animations.play(_get_direction())
@@ -62,10 +62,10 @@ func _physics_process(delta):
 	_update_animation()
 
 func _resetPos() -> void:
-	_start_pos = _tether_pos
+	start_pos = tether_pos
 
 func _get_rand_range_threshold():
-	return _rng.randi_range(-tile_move_dist, tile_move_dist)
+	return rng.randi_range(-tile_move_dist, tile_move_dist)
 	
 func _get_direction():
 	var animation_string = "walk_right"
@@ -84,18 +84,18 @@ func _get_direction():
 			animation_string = "walk_right"
 	return animation_string
 
-var _timer = 0
+var timer = 0
 
 func _process(delta):
 	if(!enemy_raycast.is_colliding()):
-		_timer = 0
+		timer = 0
 		return	
-	if _timer == 0:
+	if timer == 0:
 		_on_enemy_raycast_fire()
-	elif (_timer>fire_rate):
+	elif (timer>fire_rate):
 		_on_enemy_raycast_fire()
-		_timer = 0
-	_timer+=delta
+		timer = 0
+	timer+=delta
 
 func _on_enemy_raycast_fire() -> void:
 	var projectile = projectilePath.instantiate() 
