d608fff35a9d42f0ed8b1f1009c672c251adda12
JohnY <johnyak381@gmail.com>
2023-07-09 01:30:52 -0400

    attempt at clamp. reverted to old method

--- godot project/projectiles/projectile.gd
+++ godot project/projectiles/projectile.gd
@@ -4,21 +4,15 @@ extends Area2D
 @export var velocity: Vector2
 var screen_size
 var start_position = position
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	self.monitoring = true
 	start_position = position
 	screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var tmp_position = position
 	position = tmp_position - velocity * speed
-	if (position.x > start_position.x + 500) or (position.y > start_position.y + 500):
+	if (position.x > start_position.x + screen_size.x) or (position.y > start_position.y + 500):
 		self.queue_free()
 
-func _on_area_entered(area):
-	print(area)
-
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	print(local_shape_index)
