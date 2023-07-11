d5cb729388be8a4f23980d4d0a671c876a51f2a9
sraven24 <ksanders236@gmail.com>
2023-07-10 21:16:02 -0400

    fixed delta of projectile speed

--- godot project/projectiles/projectile.gd
+++ godot project/projectiles/projectile.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 5
+@export var speed = 200
 @export var velocity: Vector2
 var screen_size
 var start_position = position
@@ -11,8 +11,12 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var tmp_position = position
-	position = tmp_position - velocity * speed
+	#var tmp_position = position
+	#position = tmp_position - velocity * speed
+	#if (position.x > start_position.x + screen_size.x) or (position.y > start_position.y + 500):
+		#self.queue_free()
+	position += transform.x * speed * delta
 	if (position.x > start_position.x + screen_size.x) or (position.y > start_position.y + 500):
 		self.queue_free()
+
 
