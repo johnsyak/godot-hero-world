c087f6c0c0feb31fbce8f360bf3e4a0452e1eff3
JohnY <johnyak381@gmail.com>
2023-07-17 19:41:45 -0400

    rename directory for base enemy

--- godot project/enemies/enemy/hitbox_component.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-class_name hitbox_component
-
-@export var health_component: health_component
-
-func damage(attack: Attack):
-	if health_component:
-		health_component.damage(attack)
