5401b5179046eed238b03682e76d3616dd224a50
JohnY <johnyak381@gmail.com>
2023-07-17 23:25:10 -0400

    camera fixed, and position correct

--- godot project/scenes/Doorway.gd
+++ godot project/scenes/Doorway.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 
 func _on_body_entered(body: CharacterBody2D):
-	StageManager.change_stage(StageManager.BUILDING, Vector2(72, 110))
+	StageManager.change_stage(StageManager.BUILDING, Vector2(-125, -100))
 
 func _on_body_exited(body: CharacterBody2D):
 	pass
